e05051ae9ecb7c7e4fed39e437a6c4548a9e7bf7
Furkan Akçil <akcil16itu@gmail.com>
2023-07-18 13:33:17 +0300

    Update CartDetail.js

--- book-app/src/components/header/CartDetail.js
+++ book-app/src/components/header/CartDetail.js
@@ -1,93 +1,78 @@
-// import React, { Component } from "react";
-// import {
-//     UncontrolledDropdown,
-//     DropdownToggle,
-//     DropdownMenu,
-//     DropdownItem,
-//     NavItem,
-//     NavLink,
-//     Badge,
-// } from "reactstrap";
-// import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
-// import * as cartActions from "../../redux/actions/cartActions";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {
-//   faHome,
-//   faUser,
-//   faShoppingCart,
-// } from '@fortawesome/free-solid-svg-icons'
-// import { Link } from "react-router-dom";
-// import alertify from "alertifyjs";
+import React, { Component } from "react";
+import * as categoryActions from "../../redux/actions/categoryActions";
+import {
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavItem,
+    NavLink,
+    Badge,
+} from "reactstrap";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHome,
+  faUser,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom";
+import alertify from "alertifyjs";
 
-// class CartDetail extends Component {
-//     removeFromCart(product) {
-//         this.props.actions.removeFromCart(product);
-//         alertify.error(product.productName + " removed from cart!");
-//     }
-//     renderEmpty() {
-//         return (
-//             <NavItem>
-                
+class CartDetail extends Component {
+    componentDidMount(){
+        this.props.actions.getCategories();
+    }
 
-//                 <NavLink> 
-//                 <FontAwesomeIcon icon={faShoppingCart} /> 
-//                 Sepet Boş</NavLink>
-//             </NavItem>
-//         );
-//     }
+    
 
-//     rednerSummary() {
-//         return (
-//             <UncontrolledDropdown nav inNavbar>
-//                 <DropdownToggle nav caret style={{ textDecoration: 'none', color: 'white' }}>
-//                 <FontAwesomeIcon icon={faShoppingCart} style={{ textDecoration: 'none', color: 'white' }}/> 
-//                     Sepet
-//                 </DropdownToggle>
-//                 <DropdownMenu right>
-//                     {this.props.cart.map((cartItem) => (
-//                         <DropdownItem key={cartItem.product.id}>
-//                             <Badge
-//                                 color="danger"
-//                                 onClick={() => this.removeFromCart(cartItem.product)}
-//                             >
-//                                 X
-//                             </Badge>
-//                             {cartItem.product.productName}
-//                             <Badge color="success"> {cartItem.quantity}</Badge>
-//                         </DropdownItem>
-//                     ))}
+    render() {
+        const selectCategory=(category)=>{
+            console.log(category);
+            this.props.currentCategory = category;
 
-//                     <DropdownItem divider />
-//                     <DropdownItem>
-//                         <Link to="/cart">Go to Cart</Link>
-//                     </DropdownItem>
-//                 </DropdownMenu>
-//             </UncontrolledDropdown>
-//         );
-//     }
+        }
+        return (
+            <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret style={{ textDecoration: 'none', color: 'white' }}>
+                <FontAwesomeIcon icon={faShoppingCart} style={{ textDecoration: 'none', color: 'white' }}/> 
+                    Categories
+                </DropdownToggle>
+                <DropdownMenu right>
+                    {this.props.categories.map((category) => (
+                        <DropdownItem key={category.id}>
+                            
+                            <Link to={"/category/"+category.id} style={{ textDecoration: 'none', color: 'black' }} onClick={()=>selectCategory(category)}>
+                                <Badge color="success" style={{ marginLeft: '10px' }}>
+                                    {category.name}
+                                </Badge>
+                            </Link>
+                            
+                        </DropdownItem>
+                    ))}
+                </DropdownMenu>
+            </UncontrolledDropdown>
+        );
+    }
+}
 
-//     render() {
-//         return (
-//             <div>
-//                 {this.props.cart.length > 0 ? this.rednerSummary() : this.renderEmpty()}
-//             </div>
-//         );
-//     }
-// }
+function mapStateToProps(state) {
+    return {
+        categories: state.categoryReducer,
+        currentCategory: state.changeCategoryReducer
+    };
+}
 
-// function mapStateToProps(state) {
-//     return {
-//         cart: state.cartReducer,
-//     };
-// }
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            getCategories: bindActionCreators(
+                categoryActions.getCategories,
+                dispatch
+            ),
+        },
+    };
+}
 
-// function mapDispatchToProps(dispatch) {
-//     return {
-//         actions: {
-//             removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch),
-//         },
-//     };
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CartDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(CartDetail);
